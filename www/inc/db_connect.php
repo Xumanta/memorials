08a6f4d9ce143f17566a4444488433d90414d277
Xumanta <xumanta98@gmail.com>
2015-11-07 20:19:53 +0100

    * MySQL Connection no more object orientatet * Continue of Input Script

--- www/inc/db_connect.php
+++ www/inc/db_connect.php
@@ -1,4 +1,4 @@
 <?php
 include "../config.php";
-$db = new mysqli_connect(Config.DB_HOST, Config.DB_USER, Config.DB_PASS, Config.DB_DATABASE, Config.DB_PORT) or die;
+$db = mysqli_connect(Config.DB_HOST, Config.DB_USER, Config.DB_PASS, Config.DB_DATABASE, Config.DB_PORT);
 ?>
