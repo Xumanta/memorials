69512ff53db327aaacc0d3a976c49294330260d6
Steven Wobser <steven.wobser@gmail.com>
2015-11-12 18:56:46 +0100

    * Implemented LightBox

--- www/config.php
+++ www/config.php
@@ -10,9 +10,6 @@ class Config{
     const DB_USER = 'root';
     const DB_PASS = '';
 
-    const URL_BOOTSTRAP = 'http://localhost/memorials/bootstrap/';
-    const URL_JQUERY = 'http://localhost/memorials/js/jquery-1.11.3.js';
-
     const DEBUG_ENABLED = true;
 }
 ?>
